174699cf66fbcb0c94de7157da80f8699e6713aa
batval <karampish@gmail.com>
2020-06-08 15:04:23 +0300

    Added User and users.ftl

--- src/main/java/com/batval/controller/MainController.java
+++ src/main/java/com/batval/controller/MainController.java
@@ -1,11 +1,16 @@
 package com.batval.controller;
 
+import com.batval.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.List;
+
 
 @Controller
 public class MainController {
@@ -20,5 +25,15 @@ public class MainController {
     @ResponseBody
     public String raw() {
         return "Raw data";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        Collection<User> users = List.of(
+          new User("Ivan", "Ivanov", "ivan.ivanov@gmail.com"),
+                new User("Petr","Petrov","petr.petrov@gmail.com")
+        );
+        model.addAttribute("users",users);
+        return "/users";
     }
 }
---
+++ src/main/java/com/batval/model/User.java
@@ -0,0 +1,25 @@
+package com.batval.model;
+
+public class User {
+    private final String name;
+    private final String surName;
+    private final String email;
+
+    public User(String name, String surName, String email) {
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
